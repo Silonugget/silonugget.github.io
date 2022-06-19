15c96cf4dc08eef5c42c2b3061c459cc3c39c4f4
Silonugget <107784929+Silonugget@users.noreply.github.com>
2022-06-19 00:58:46 -0600

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image();
 img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
-img.src = "images/basuda.png"
+img.src = "images/basuda.PNG"
 // general settings
 let gamePlaying = false;
 const gravity = .45;
